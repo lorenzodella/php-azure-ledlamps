f285c9a9cf3d9ff215b9c390f7be9cbcc4179715
Lorenzo Della Matera <58138595+lorenzodella@users.noreply.github.com>
2022-12-23 19:31:18 +0100

    changedb

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 	# ----------- variabili Globali --------
 	//ini_set('display_errors','On'); error_reporting(E_ALL); // set errori php
 
-	$host = "lorenzodellamatera.northeurope.cloudapp.azure.com";
+	$host = "20.223.202.42";
 	$dbUser = "root";
 	$dbPwd  = "D_ellamateral_0";
 	$dbName	  = "ledlamps";
