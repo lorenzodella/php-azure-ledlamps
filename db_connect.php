99cee8d59b626857ecefdeef3cebd92297697ac3
Lorenzo Della Matera <58138595+lorenzodella@users.noreply.github.com>
2022-12-24 19:30:48 +0100

    db ssl

--- db_connect.php
+++ db_connect.php
@@ -8,7 +8,7 @@
 	$dbName	  = "ledlamps";
 
 	$con = mysqli_init();
-	mysqli_ssl_set($con,NULL,NULL, "../ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+	mysqli_ssl_set($con,NULL,NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 	mysqli_real_connect($host, $dbUser, $dbPwd, $dbName, 3306, MYSQLI_CLIENT_SSL);
     if(!$con)
     {
