cb45e9f815a8eac3f60dcb18c456c494227db337
Lorenzo Della Matera <58138595+lorenzodella@users.noreply.github.com>
2022-12-24 19:39:45 +0100

    test

--- db_connect.php
+++ db_connect.php
@@ -10,7 +10,7 @@
 	$con = mysqli_init();
 	mysqli_ssl_set($con,NULL,NULL, "/ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 	mysqli_real_connect($con, $host, $dbUser, $dbPwd, $dbName, 3306, MYSQLI_CLIENT_SSL);
-    if(mysqli_connect_errno($conn))
+    if(mysqli_connect_errno($con))
     {
         die("Connection falied".mysqli_connect_error());
     }
