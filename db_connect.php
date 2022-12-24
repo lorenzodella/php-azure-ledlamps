82daa7c4a30e4e90a128772b588ac5c8a25dffb3
Lorenzo Della Matera <58138595+lorenzodella@users.noreply.github.com>
2022-12-24 19:53:53 +0100

    db ssl

--- db_connect.php
+++ db_connect.php
@@ -7,14 +7,14 @@
 	$dbPwd  = "D_ellamateral_0";
 	$dbName	  = "ledlamps";
 
-	echo file_get_contents( "ssl/file.txt" );
+	//echo file_get_contents( "ssl/file.txt" );
 
-	/*$con = mysqli_init();
-	mysqli_ssl_set($con,NULL,NULL, "/ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+	$con = mysqli_init();
+	mysqli_ssl_set($con,NULL,NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 	mysqli_real_connect($con, $host, $dbUser, $dbPwd, $dbName, 3306, MYSQLI_CLIENT_SSL);
     if(mysqli_connect_errno($con))
     {
         die("Connection falied".mysqli_connect_error());
     }
-	echo "Connected successfully";*/
+	echo "Connected successfully";
 ?>
