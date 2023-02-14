e858852f02dd192257d7e0b9309188d4a99ee2b6
Lorenzo Della Matera <58138595+lorenzodella@users.noreply.github.com>
2023-02-14 12:58:18 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -8,8 +8,6 @@
 	$dbName	  = "ledlamps";
 
 	//echo file_get_contents( "ssl/file.txt" );
-	echo __DIR__;
-	echo $_SERVER['DOCUMENT_ROOT'];
 
 	$con = mysqli_init();
 	mysqli_ssl_set($con,NULL,NULL, $_SERVER['DOCUMENT_ROOT']."/ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
