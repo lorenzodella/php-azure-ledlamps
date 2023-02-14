8904f81697034c4a33d988b1d9cfa3bfc4c2cbd5
Lorenzo Della Matera <58138595+lorenzodella@users.noreply.github.com>
2023-02-14 12:49:49 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -10,7 +10,7 @@
 	//echo file_get_contents( "ssl/file.txt" );
 
 	$con = mysqli_init();
-	mysqli_ssl_set($con,NULL,NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+	mysqli_ssl_set($con,NULL,NULL, $_SERVER['DOCUMENT_ROOT']."ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 	mysqli_real_connect($con, $host, $dbUser, $dbPwd, $dbName, 3306, MYSQLI_CLIENT_SSL);
     if(mysqli_connect_errno())
     {
